6facfc9ef2ec39f197c90b1148ff6603b69bdac4
Yi Zhang <yi@bravenewcoin.com>
2020-04-13 19:37:45 +1200

    Upload and download image to S3

---
+++ src/main/java/com/yi/awsimageupload/filestore/FileStore.java
@@ -0,0 +1,49 @@
+package com.yi.awsimageupload.filestore;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileStore {
+
+    private final AmazonS3 s3;
+
+    @Autowired
+    public FileStore(AmazonS3 s3) {
+        this.s3 = s3;
+    }
+
+    public void save(String path, String fileName, Optional<Map<String, String>> optionalMetadata, InputStream inputStream) {
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            optionalMetadata.ifPresent(map -> {
+                if (!map.isEmpty()) {
+                    map.forEach(metadata::addUserMetadata);
+                }
+            });
+            s3.putObject(path, fileName, inputStream, metadata);
+        } catch (AmazonServiceException ex) {
+            throw new IllegalStateException("Failed to store file to s3", ex);
+        }
+    }
+
+    public byte[] download(String path, String key) {
+        try {
+            S3Object object = s3.getObject(path, key);
+            S3ObjectInputStream inputStream = object.getObjectContent();
+            return IOUtils.toByteArray(inputStream);
+        } catch (AmazonServiceException | IOException ex) {
+            throw new IllegalStateException("Failed to download file to s3", ex);
+        }
+    }
+}
