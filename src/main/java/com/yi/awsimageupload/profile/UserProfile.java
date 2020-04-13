b2b4ac3a7790dec8720274d3d71c11956e6103d5
Yi Zhang <yi@bravenewcoin.com>
2020-04-13 19:35:29 +1200

    Define user profile data model

---
+++ src/main/java/com/yi/awsimageupload/profile/UserProfile.java
@@ -0,0 +1,54 @@
+package com.yi.awsimageupload.profile;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserProfile {
+
+    private final UUID userProfileId;
+    private final String username;
+    private String userProfileImageLink; // S3 Key
+
+    public UserProfile(UUID userProfileId, String username, String userProfileImageLink) {
+        this.userProfileId = userProfileId;
+        this.username = username;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public UUID getUserProfileId() {
+        return userProfileId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Optional<String> getUserProfileImageLink() {
+        return Optional.ofNullable(userProfileImageLink);
+    }
+
+    public UserProfile setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserProfile that = (UserProfile) o;
+        return com.google.common.base.Objects.equal(userProfileId, that.userProfileId) &&
+            com.google.common.base.Objects.equal(username, that.username) &&
+            com.google.common.base.Objects.equal(userProfileImageLink, that.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, username, userProfileImageLink);
+    }
+}
